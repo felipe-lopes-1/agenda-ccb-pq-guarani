fbd5f0932593eb5521d882bbffc7d2cb777fe660
felipe-lopes-1 <felopes.silva.77@gmail.com>
2025-05-26 18:11:05 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    /**
+     * Botão Voltar ao Topo
+     */
+    const $btn = $('#backToTop');
+    // Mostra/esconde o botão conforme rolagem
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $btn.fadeIn();
+        } else {
+            $btn.fadeOut();
+        }
+    });
+
+    // Anima rolagem até o topo
+    $btn.click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 0);
+        return false;
+    });
+
+    /**
+     * Alternar entre os temas Claro e Escuro
+     */
+    $('#toggleTheme').click(function () {
+        $('body').toggleClass('dark-mode');
+
+        // Troca o ícone do botão
+        const icon = $(this).find('i');
+        if ($('body').hasClass('dark-mode')) {
+            icon.removeClass('fa-moon-o').addClass('fa-sun-o');
+            // Altere o logo, se necessário
+            $('img[src="logo-ccb-light.png"]').attr('src', 'logo-ccb-dark.png');
+        } else {
+            icon.removeClass('fa-sun-o').addClass('fa-moon-o');
+            $('img[src="logo-ccb-dark.png"]').attr('src', 'logo-ccb-light.png');
+        }
+    });
+});
